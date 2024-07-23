e4e92ecb8f1ec2d5ee3ced832e38ac4d9909255f
KiRill <k.stulov@inbox.ru>
2024-07-23 23:28:25 +0500

    feat: deleted all migrations again to fix all in 5 migrations according to mentor comments. Also did rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_22_140427) do
-  create_table "answers", force: :cascade do |t|
-    t.text "body", limit: 1000, null: false
-    t.boolean "correct", default: false
-    t.integer "question_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title", limit: 60, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "title", limit: 100, null: false
-    t.text "body", limit: 600, null: false
-    t.integer "test_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 1
-    t.integer "category_id"
-    t.integer "test_author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name", limit: 50, null: false
-    t.string "email", limit: 40, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
