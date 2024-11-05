5c3df3e19435e8492dfd87a0f253eaff2152bf1d
KiRill <k.stulov@inbox.ru>
2024-11-05 14:49:07 +0500

    fix: changed create! method in seeds.rb to find_or_create_by! to prevent duplicate records if run seeds another time

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,29 @@
 #   end
 
 # Create Categories
-programming = Category.create!(title: 'Programming') # is a class method provided by Active Record, Rails' ORM.
-web_dev = Category.create!(title: 'Web Development')
+programming = Category.find_or_create_by!(title: 'Programming') # is a class method provided by Active Record, Rails' ORM.
+web_dev = Category.find_or_create_by!(title: 'Web Development')
 
 # Create Users
-john = User.create!(name: 'John', email: 'john@smth.com')
-jane = User.create!(name: 'Jane', email: 'jane@smth.com')
+john = User.find_or_create_by!(name: 'John', email: 'john@smth.com')
+jane = User.find_or_create_by!(name: 'Jane', email: 'jane@smth.com')
 
 # Create Tests
-ruby_test = Test.create!(title: 'Ruby Basics', level: 1, category: programming, author: john)
-rails_test = Test.create!(title: 'Rails Fundamentals', level: 1, category: web_dev, author: jane)
-ruby_test2 = Test.create!(title: 'Ruby Advanced', level: 2, category: programming, author: john)
+ruby_test = Test.find_or_create_by!(title: 'Ruby Basics', level: 1, category: programming, author: john)
+rails_test = Test.find_or_create_by!(title: 'Rails Fundamentals', level: 1, category: web_dev, author: jane)
+ruby_test2 = Test.find_or_create_by!(title: 'Ruby Advanced', level: 2, category: programming, author: john)
 
 # Create questions
-q1 = Question.create!(body: 'What is instance method in Ruby?', test: ruby_test)
-q2 = Question.create!(body: 'What is MVC in Rails?', test: rails_test)
+q1 = Question.find_or_create_by!(body: 'What is instance method in Ruby?', test: ruby_test)
+q2 = Question.find_or_create_by!(body: 'What is MVC in Rails?', test: rails_test)
 
 # Create answers
-Answer.create!(body: 'Is a type of variable which starts with an @ symbol', correct: true, question: q1)
-Answer.create!(body: 'There is only class variable in Ruby', correct: false, question: q1)
-Answer.create!(body: 'Model-View-Controller architecture', correct: true, question: q2)
-Answer.create!(body: 'Multiple Virtual Comp', correct: false, question: q2)
+Answer.find_or_create_by!(body: 'Is a type of variable which starts with an @ symbol', correct: true, question: q1)
+Answer.find_or_create_by!(body: 'There is only class variable in Ruby', correct: false, question: q1)
+Answer.find_or_create_by!(body: 'Model-View-Controller architecture', correct: true, question: q2)
+Answer.find_or_create_by!(body: 'Multiple Virtual Comp', correct: false, question: q2)
 
 # Create UserTests (tests passed by users)
-UserTest.create!(status: 'completed', user: john, test: ruby_test)
-UserTest.create!(status: 'in_progress', user: jane, test: rails_test)
-UserTest.create!(status: 'completed', user: john, test: ruby_test2)
+UserTest.find_or_create_by!(status: 'completed', user: john, test: ruby_test)
+UserTest.find_or_create_by!(status: 'in_progress', user: jane, test: rails_test)
+UserTest.find_or_create_by!(status: 'completed', user: john, test: ruby_test2)
